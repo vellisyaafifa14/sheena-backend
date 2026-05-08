f0c0de22fb8140f695be24a6ed0819e01f02bff1
vellisyaafifa14 <167750217+vellisyaafifa14@users.noreply.github.com>
2026-05-09 03:16:52 +0700

    login integration (no debug)

--- app/Http/Controllers/Api/AdminAuthController.php
+++ app/Http/Controllers/Api/AdminAuthController.php
@@ -18,17 +18,24 @@ class AdminAuthController extends Controller
 
         $admin = Admin::where('email', $request->email)->first();
 
-       return response()->json([
-    'input_password' => $request->password,
-    'hash' => $admin->password,
-    'check' => Hash::check($request->password, $admin->password),
-]);
-
 if (!$admin || !Hash::check($request->password, $admin->password)) {
     return response()->json([
         'message' => 'Email atau password salah'
     ], 401);
 }
+
+$token = $admin->createToken('admin-token')->plainTextToken;
+
+return response()->json([
+    'message' => 'Login berhasil',
+    'token' => $token,
+    'data' => [
+        'id_admin' => $admin->id_admin,
+        'name' => $admin->name,
+        'email' => $admin->email,
+    ]
+], 200);
+
 
         $token = $admin->createToken('admin-token')->plainTextToken;
 
