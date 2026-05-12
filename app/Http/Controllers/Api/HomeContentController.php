fcb59b9e6bb1c42344e1226cdc9de358d69d7ff4
vellisyaafifa14 <167750217+vellisyaafifa14@users.noreply.github.com>
2026-05-12 07:00:53 +0700

    add home content image upload

--- app/Http/Controllers/Api/HomeContentController.php
+++ app/Http/Controllers/Api/HomeContentController.php
@@ -12,17 +12,44 @@ class HomeContentController extends Controller
     {
         return response()->json([
             'success' => true,
-            'data' => HomeContent::pluck('value', 'key'),
+            'data' => HomeContent::pluck('value', 'key')->toArray(),
         ]);
     }
 
     public function update(Request $request)
     {
-        foreach ($request->all() as $key => $value) {
+        foreach ($request->except([
+            'hero_image',
+            'section2_image_1',
+            'section2_image_2',
+            'section2_image_3',
+            'section2_image_4',
+            'promo_image',
+        ]) as $key => $value) {
             HomeContent::updateOrCreate(
                 ['key' => $key],
                 ['value' => $value]
             );
+        }
+
+        $imageFields = [
+            'hero_image',
+            'section2_image_1',
+            'section2_image_2',
+            'section2_image_3',
+            'section2_image_4',
+            'promo_image',
+        ];
+
+        foreach ($imageFields as $field) {
+            if ($request->hasFile($field)) {
+                $path = $request->file($field)->store('home-content', 'public');
+
+                HomeContent::updateOrCreate(
+                    ['key' => $field],
+                    ['value' => '/storage/' . $path]
+                );
+            }
         }
 
         return response()->json([
